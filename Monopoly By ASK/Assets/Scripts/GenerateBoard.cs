7445abc5d38bb8e429b73251f42e8f63913b6efb
Aaron Vischer <asurnith@gmail.com>
2019-03-05 11:40:48 -0800

    Update

---
+++ Monopoly By ASK/Assets/Scripts/GenerateBoard.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Audio.Google;
+
+public class GenerateBoard : MonoBehaviour
+{
+    public GameObject tile = new GameObject();
+    public GameObject cornerTile = new GameObject();
+    public GameObject[] tiles = new GameObject[36];
+    public GameObject[] cornerTiles = new GameObject[4];
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        for (int i = 0; i < tiles.Length; ++i)
+        {
+
+            GameObject gameTile = Instantiate(tile);
+            gameTile.transform.position = transform.position;
+
+        }
+
+        for (int i = 0; i < cornerTiles.Length; ++i)
+        {
+            
+            
+            
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
