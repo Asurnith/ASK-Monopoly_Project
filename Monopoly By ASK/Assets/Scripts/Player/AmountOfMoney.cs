efb6d6550d5525df4ea5438f72ccda711643c928
Santiago Franco <santiagofranco3321@gmail.com>
2019-04-03 10:58:06 -0700

    Auto stash before merge of "master" and "origin/master"

---
+++ Monopoly By ASK/Assets/Scripts/Player/AmountOfMoney.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmountOfMoney : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int initialPlayerAmount = 0;
+        int[][] totalBills= new int[7][2]{
+            (1,0),
+                
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
