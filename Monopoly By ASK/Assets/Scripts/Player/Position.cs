b3355f21acba7213f7edd9abda6b1f967bcf7fc2
Santiago Franco <santiagofranco3321@gmail.com>
2019-04-16 10:05:51 -0700

    Work on Player
    Position, Cube, etc.

--- Monopoly By ASK/Assets/Scripts/Player/Position.cs
+++ Monopoly By ASK/Assets/Scripts/Player/Position.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Position : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int currentIndex { get; set; }
+    public void MovePlayer(int spaces)
     {
-        
+        currentIndex += spaces;
+        CheckGo(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void MoveToTile(int tile, bool doNotPassGo)
     {
-        
+        if (currentIndex > tile)
+            currentIndex = 40 + tile;
+        else
+            currentIndex = tile;
+        CheckGo(doNotPassGo);
+    }
+
+    private void CheckGo(bool doNotPassGo)
+    {
+        if (currentIndex >= 40 && !doNotPassGo)
+            GetComponent<AmountOfMoney>().ChangeBalence(200);
+        currentIndex %= 40;
     }
 }
