18afce5d66acc47497584ed4c42a616399af4d2e
Santiago Franco <santiagofranco3321@gmail.com>
2019-04-18 12:13:10 -0700

    roll dice

---
+++ Monopoly By ASK/Assets/Scripts/GameMangager/Dice.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dice : MonoBehaviour
+{
+    private int _totalValue;
+    private int _diceOneValue, _diceTwoValue;
+    [SerializeField] private GameObject dieOne, dieTwo;
+    [SerializeField] private List<Sprite> _dieFaces;
+
+    
+    public int Roll()
+    {
+        StartCoroutine(RollAnim());
+        return _totalValue;
+    }
+
+    private IEnumerator RollAnim()
+    {         
+        for (int i = 0; i < Random.Range(10, 21); i++)
+        {
+            _diceOneValue = Random.Range(0, 6) + 1;
+            dieOne.GetComponent<SpriteRenderer>().sprite = _dieFaces[_diceOneValue - 1]; 
+            _diceTwoValue = Random.Range(0, 6) + 1;
+            dieTwo.GetComponent<SpriteRenderer>().sprite = _dieFaces[_diceTwoValue - 1];
+            yield return new WaitForSeconds(.25f);
+        }
+
+        _totalValue = _diceOneValue + _diceTwoValue;
+    }
+}
--- Monopoly By ASK/Assets/Scripts/GameMangager/GameLoop.cs
+++ Monopoly By ASK/Assets/Scripts/GameMangager/GameLoop.cs
@@ -5,16 +5,16 @@ using UnityEngine;
 public class GameLoop : MonoBehaviour
 {
 
-    public string hi;
+    [SerializeField] private List<GameObject> _players;
+  
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
